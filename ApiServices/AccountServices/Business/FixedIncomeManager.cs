3cdf1e66a6e55016c3181cd502e9b0823d3d39d8
Sebastian Pardo <js.pardo.j@gmail.com>
2024-03-06 16:44:10 -0500

    Added FixedIncome Manager

---
+++ ApiServices/AccountServices/Business/FixedIncomeManager.cs
@@ -0,0 +1,45 @@
+﻿using AccountServices.Business.Interfaces;
+using OhMyMoney.DataCore.Data;
+using OhMyMoney.DataCore.Entities;
+
+namespace AccountServices.Business
+{
+    public class FixedIncomeManager : IFixedIncomeManager
+    {
+        DatabaseContext context;
+        public FixedIncomeManager(DatabaseContext context)
+        {
+            this.context = context;
+        }
+
+        public FixedIncome Add(FixedIncome fixedIncome)
+        {
+            fixedIncome = context.FixedIncome.Add(fixedIncome).Entity;
+            context.SaveChanges();
+            return fixedIncome;
+        }
+
+        public bool Delete(FixedIncome fixedIncome)
+        {
+            context.FixedIncome.Remove(fixedIncome);
+            return context.SaveChanges() > 0;
+        }
+
+        public IEnumerable<FixedIncome> GetAllByUserId(Guid userId)
+        {
+            return context.FixedIncome.Where(fe => fe.UserId == userId);
+        }
+
+        public FixedIncome GetById(Guid id)
+        {
+            return context.FixedIncome.FirstOrDefault(x => x.Id == id);
+        }
+
+        public bool Update(FixedIncome fixedIncome)
+        {
+            context.FixedIncome.Update(fixedIncome);
+            context.SaveChanges();
+            return context.SaveChanges() > 0;
+        }
+    }
+}
---
+++ ApiServices/AccountServices/Business/Interfaces/IFixedIncomeManager.cs
@@ -0,0 +1,13 @@
+﻿using OhMyMoney.DataCore.Entities;
+
+namespace AccountServices.Business.Interfaces
+{
+    public interface IFixedIncomeManager
+    {
+        IEnumerable<FixedIncome> GetAllByUserId(Guid userId);
+        FixedIncome GetById(Guid id);
+        FixedIncome Add(FixedIncome user);
+        bool Update(FixedIncome user);
+        bool Delete(FixedIncome user);
+    }
+}
