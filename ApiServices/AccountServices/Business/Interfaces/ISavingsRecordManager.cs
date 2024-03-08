f7be4530b00c2a76b414bc953bb1c2dca3823da9
Sebastian Pardo <js.pardo.j@gmail.com>
2024-03-08 16:57:05 -0500

    SavingsRecordManager

---
+++ ApiServices/AccountServices/Business/Interfaces/ISavingsRecordManager.cs
@@ -0,0 +1,13 @@
+﻿using OhMyMoney.DataCore.Entities;
+
+namespace AccountServices.Business.Interfaces
+{
+    public interface ISavingsRecordManager
+    {
+        IEnumerable<SavingsRecord> GetAllByUserId(Guid userId);
+        SavingsRecord GetById(Guid id);
+        SavingsRecord Add(SavingsRecord savingsRecord);
+        bool Update(SavingsRecord savingsRecord);
+        bool Delete(SavingsRecord savingsRecord);
+    }
+}
---
+++ ApiServices/AccountServices/Business/SavingsRecordManager.cs
@@ -0,0 +1,45 @@
+﻿using AccountServices.Business.Interfaces;
+using OhMyMoney.DataCore.Data;
+using OhMyMoney.DataCore.Entities;
+
+namespace AccountServices.Business
+{
+    public class SavingsRecordManager : ISavingsRecordManager
+    {
+        DatabaseContext context;
+        public SavingsRecordManager(DatabaseContext context)
+        {
+            this.context = context;
+        }
+
+        public SavingsRecord Add(SavingsRecord savingsRecord)
+        {
+            savingsRecord = context.SavingsRecord.Add(savingsRecord).Entity;
+            context.SaveChanges();
+            return savingsRecord;
+        }
+
+        public bool Delete(SavingsRecord savingsRecord)
+        {
+            context.SavingsRecord.Remove(savingsRecord);
+            return context.SaveChanges() > 0;
+        }
+
+        public IEnumerable<SavingsRecord> GetAllByUserId(Guid userId)
+        {
+            return context.SavingsRecord.Where(e => e.Id == userId);
+        }
+
+        public SavingsRecord GetById(Guid id)
+        {
+            return context.SavingsRecord.FirstOrDefault(x => x.Id == id);
+        }
+
+        public bool Update(SavingsRecord savingsRecord)
+        {
+            context.SavingsRecord.Update(savingsRecord);
+            context.SaveChanges();
+            return context.SaveChanges() > 0;
+        }
+    }
+}
