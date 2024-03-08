60e2341cbb882bb716fc642aaf4eab49bea89f97
Sebastian Pardo <js.pardo.j@gmail.com>
2024-03-08 17:08:11 -0500

    UserManager

---
+++ ApiServices/AccountServices/Business/Interfaces/IUserManager.cs
@@ -0,0 +1,15 @@
+﻿using OhMyMoney.DataCore.Entities;
+
+namespace AccountServices.Business.Interfaces
+{
+    public interface IUserManager
+    {
+        User? getByUserNameOrEmail(string accountEmail);
+        IEnumerable<User> GetAll();
+        User GetById(Guid id);
+        bool Add(User user);
+        User Update(User user);
+        bool Delete(User user);
+
+    }
+}
