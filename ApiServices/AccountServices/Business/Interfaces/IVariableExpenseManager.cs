9da238c17ddd1dbbaa02a0b705c845facb01c926
Sebastian Pardo <js.pardo.j@gmail.com>
2024-03-08 17:18:22 -0500

    VariableExpensesManager

---
+++ ApiServices/AccountServices/Business/Interfaces/IVariableExpenseManager.cs
@@ -0,0 +1,13 @@
+﻿using OhMyMoney.DataCore.Entities;
+
+namespace AccountServices.Business.Interfaces
+{
+    public interface IVariableExpenseManager
+    {
+        IEnumerable<VariableExpense> GetAllByUserId(Guid userId);
+        VariableExpense GetById(Guid id);
+        VariableExpense Add(VariableExpense variableExpense);
+        bool Update(VariableExpense variableExpense);
+        bool Delete(VariableExpense variableExpense);
+    }
+}
---
+++ ApiServices/AccountServices/Business/VariableExpenseManager.cs
@@ -0,0 +1,45 @@
+﻿using AccountServices.Business.Interfaces;
+using OhMyMoney.DataCore.Data;
+using OhMyMoney.DataCore.Entities;
+
+namespace AccountServices.Business
+{
+    public class VariableExpenseManager : IVariableExpenseManager
+    {
+        DatabaseContext context;
+        public VariableExpenseManager(DatabaseContext context)
+        {
+            this.context = context;
+        }
+
+        public VariableExpense Add(VariableExpense variableExpense)
+        {
+            variableExpense = context.VariableExpense.Add(variableExpense).Entity;
+            context.SaveChanges();
+            return variableExpense;
+        }
+
+        public bool Delete(VariableExpense variableExpense)
+        {
+            context.VariableExpense.Remove(variableExpense);
+            return context.SaveChanges() > 0;
+        }
+
+        public bool Update(VariableExpense variableExpense)
+        {
+            context.VariableExpense.Update(variableExpense);
+            context.SaveChanges();
+            return context.SaveChanges() > 0;
+        }
+
+        IEnumerable<VariableExpense> GetAllByUserId(Guid userId)
+        {
+            return context.VariableExpense.Where(fe => fe.ExpensesBudget.UserId == userId);
+        }
+
+        VariableExpense GetById(Guid id)
+        {
+            return context.VariableExpense.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
