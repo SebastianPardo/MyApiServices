290ec999de4c1acdce39a0e96e78e2f8c6c95f22
Sebastian Pardo <js.pardo.j@gmail.com>
2024-03-08 16:52:09 -0500

    SavingsPocketManager

---
+++ ApiServices/AccountServices/Business/Interfaces/ISavingsPocketManager.cs
@@ -0,0 +1,13 @@
+﻿using OhMyMoney.DataCore.Entities;
+
+namespace AccountServices.Business.Interfaces
+{
+    public interface ISavingsPocketManager
+    {
+        IEnumerable<SavingsPocket> GetAllByUserId(Guid userId);
+        SavingsPocket GetById(Guid id);
+        SavingsPocket Add(SavingsPocket savingsPocket);
+        bool Update(SavingsPocket savingsPocket);
+        bool Delete(SavingsPocket savingsPocket);
+    }
+}
---
+++ ApiServices/AccountServices/Business/SavingsPocketManager.cs
@@ -0,0 +1,47 @@
+﻿using AccountServices.Business.Interfaces;
+using OhMyMoney.DataCore.Data;
+using OhMyMoney.DataCore.Entities;
+
+namespace AccountServices.Business
+{
+    public class SavingsPocketManager : ISavingsPocketManager
+    {
+        DatabaseContext context;
+        public SavingsPocketManager(DatabaseContext context)
+        {
+            this.context = context;
+        }
+
+
+        public SavingsPocket Add(SavingsPocket savingsPocket)
+        {
+            savingsPocket = context.SavingsPocket.Add(savingsPocket).Entity;
+            context.SaveChanges();
+            return savingsPocket;
+        }
+
+        public bool Delete(SavingsPocket savingsPocket)
+        {
+            context.SavingsPocket.Remove(savingsPocket);
+            return context.SaveChanges() > 0;
+        }
+
+        public IEnumerable<SavingsPocket> GetAllByUserId(Guid userId)
+        {
+            return context.SavingsPocket.Where(e => e.Id == userId);
+        }
+
+        public SavingsPocket GetById(Guid id)
+        {
+            return context.SavingsPocket.FirstOrDefault(x => x.Id == id);
+        }
+
+        public bool Update(SavingsPocket savingsPocket)
+        {
+            context.SavingsPocket.Update(savingsPocket);
+            context.SaveChanges();
+            return context.SaveChanges() > 0;
+        }
+        
+    }
+}
